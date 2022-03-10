74283c69687610aa4c48ce78a0ac3ca4cdce0a8c
Selim Onat <selim.onat@mytaxi.com>
2022-03-10 15:12:06 +0100

    Base bot class implemented.

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import os
 
 PATH_DB = os.path.join(os.getcwd(), 'db', '.')
 PATH_DB_TIMESERIES = os.path.join(PATH_DB, 'timeseries')
-PATH_DB_BOT_HISTORY = os.path.join(PATH_DB, 'bot')
+PATH_DB_BOT_HISTORY = os.path.join(PATH_DB, 'history')
+PATH_DB_BOT_FEATURE = os.path.join(PATH_DB, 'features')
 GRANULARITY = 60*15 # 15 minutes
 START_YEAR = 2021
--- visuals.py
+++ visuals.py
@@ -2,13 +2,15 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.express as px
-from utils import read_data, list_local_products, get_logger
+from utils import read_timeseries, list_local_products, get_logger
 
 
 def fig(product_id):
-    df = read_data(product_id)
+    df = read_timeseries(product_id)
     fig = px.line(df, x='datetime', y='close', title=product_id)
     fig.update_layout(title_font_size=30)
+    # TODO: append feature traces as well.
+    # TODO: Append decisions
     return fig
 
 
@@ -17,28 +19,6 @@ app = dash.Dash(__name__)
 
 app.layout = html.Div([dcc.Graph(id=p, figure=fig(p)) for p in list_local_products()])
 
-#
-# def display_candlestick(coinname, slider_toggle):
-#
-#     if coinname:
-#         logger.info(f'{coinname[0]} selected.')
-#         df = read_data(coinname[0])
-#         logger.info(df.head(10))
-#         fig = go.Figure(go.Candlestick(
-#             x=df.index,
-#             open=df['open'],
-#             high=df['high'],
-#             low=df['low'],
-#             close=df['close']
-#         ))
-#     else:
-#         fig = go.Figure()
-#
-#     fig.update_layout(
-#         xaxis_rangeslider_visible='slider' in slider_toggle
-#     )
-#
-#     return fig
 
 
 if __name__ == "__main__":
